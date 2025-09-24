4378c81e8331cf93261acace05945dc36f8b27d9
ryntk <rayanetoko@proton.me>
2025-09-24 18:22:17 +0000

    update README.md

---
+++ main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+/**
+ * factorize - factorize number.
+ *
+ * @n: the number to factorize.
+ */
+void factorize(long n)
+{
+	long p = 2;
+
+	while (p <= n / 2 && n % p != 0)
+	{
+		p++;
+	}
+
+	if (n % p == 0)
+	{
+		printf("%ld=%ld*%ld\n", n, n / p, p);
+	}
+
+}
+
+
+/**
+ * main - entry point of the program.
+ * The program factorize a set of number in a file.
+ *
+ * @argc: the number of parameter sent to the program.
+ * @argv: an array of string representing the parameters sent.
+ * Return: 0 on success and 1 on error.
+ */
+int main(int argc, char **argv)
+{
+	long n = 0;
+	FILE *f = NULL;
+
+	if (argc < 2)
+	{
+		printf("usage: factors <file>\n");
+		return (1);
+	}
+
+	f = fopen(argv[1], "r");
+	if (f == NULL)
+	{
+		perror("Error while opening file");
+		return (1);
+	}
+
+	while (fscanf(f, "%ld\n", &n) != EOF)
+	{
+		factorize(n);
+	}
+
+	return (0);
+}
